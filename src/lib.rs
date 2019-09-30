07af064a791fd95a3f672e782a2dce3525b9b7bf
Tobias Pütz <tobias.puetz@uni-tuebingen.de>
2019-09-30 15:42:05 +0200

    Enable decoding from python.
    Enables decoding from python via exposing
    the decoder and related structs.

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,9 @@ use tagger::PyTagger;
 mod sentence;
 pub use sentence::{PySentence, PySentenceIterator, PyToken};
 
+mod decoder;
+use decoder::{PyDecoder,PyLabel};
+
 /// This is a Python module for wrapping the sticker sequence labeler.
 #[pymodule]
 fn sticker(_py: Python, m: &PyModule) -> PyResult<()> {
@@ -22,6 +25,7 @@ fn sticker(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<PySentence>()?;
     m.add_class::<PySentenceIterator>()?;
     m.add_class::<PyToken>()?;
-
+    m.add_class::<PyDecoder>()?;
+    m.add_class::<PyLabel>()?;
     Ok(())
 }
