603a07de6eeba30ea00518c230c29f4d8d84cdbc
Tobias Pütz <tobias.puetz@uni-tuebingen.de>
2019-10-03 16:44:40 +0200

    Remove tensorflow dependencies.
    Loading this module while also loading py-tensorflow leads to a
    conflict. Removing the tensorflow dependency allows to work around
    it by using `patchelf --remove-needed libtensorflow.so`.
    remove-needed seems to corrupt the shared library if tensorflow
    symbols are contained.

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,6 @@ use pyo3::prelude::*;
 mod util;
 pub use util::ListVec;
 
-mod config;
-use config::{PyConfig, PyLabeler, PyModel};
-
-mod tagger;
-use tagger::PyTagger;
-
 mod sentence;
 pub use sentence::{PySentence, PySentenceIterator, PyToken};
 
@@ -18,10 +12,6 @@ use decoder::{PyDecoder,PyLabel};
 /// This is a Python module for wrapping the sticker sequence labeler.
 #[pymodule]
 fn sticker(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_class::<PyConfig>()?;
-    m.add_class::<PyLabeler>()?;
-    m.add_class::<PyModel>()?;
-    m.add_class::<PyTagger>()?;
     m.add_class::<PySentence>()?;
     m.add_class::<PySentenceIterator>()?;
     m.add_class::<PyToken>()?;
